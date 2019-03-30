4d7cd8fcb6e606a005027247751d3f9e9e1b0413
Adella <adellakenn@gmail.com>
2019-03-30 14:19:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var a, b 
+//var a, b 
 
-beforeEach(function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
-})
+//beforeEach(function() {
+//  a = Math.floor(Math.random() * 1000)
+//  b = Math.floor(Math.random() * 1000)
+// })
 
+function add() {
+  add a + b 
+}
+
+add()
